cf1d5283e9d9b111885489af0eac68e64afe5bf4
Lucas Oliveira <42073344+Storkluc@users.noreply.github.com>
2019-04-29 16:13:41 -0300

    New Project
    This is my TCC.

---
+++ LogMath/app/src/main/java/faseslogmath/com/logmath/ComecarActivity.java
@@ -0,0 +1,16 @@
+package faseslogmath.com.logmath;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import logmath.com.logmath.R;
+
+public class ComecarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comecar);
+    }
+}
---
+++ LogMath/app/src/main/java/logmath/com/logmath/MainActivity.java
@@ -0,0 +1,38 @@
+package logmath.com.logmath;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import faseslogmath.com.logmath.ComecarActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView botaoconfig;
+    ImageView botaocomecar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaocomecar = (ImageView)findViewById(R.id.comecarId);
+        botaoconfig = (ImageView) findViewById(R.id.configId);
+
+        botaocomecar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ComecarActivity.class));
+            }
+        });
+
+        botaoconfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ConfigActivity.class));
+            }
+        });
+    }
+}
