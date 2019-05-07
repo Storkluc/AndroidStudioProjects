2130f7fa5a09d9598ad19f8d00ca059c70025d19
Lucas Oliveira <42073344+Storkluc@users.noreply.github.com>
2019-05-07 12:36:48 -0300

    Last changes.
    My projects of class of Android

---
+++ ListView/app/src/main/java/logmath/com/listview/MainActivity.java
@@ -0,0 +1,45 @@
+package logmath.com.listview;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private ListView listaItens;
+    private String[] itens = {
+      "Interface", "Lógica", "Interação", "Roteiro","Animação", "Personagens",
+      "Fases","Ícones"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listaItens = (ListView) findViewById(R.id.listViewId);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        listaItens.setAdapter( adapter );
+
+        listaItens.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int codigoPosicao = position;
+                String valorClicado = listaItens.getItemIdAtPosition(codigoPosicao).toString();
+            }
+        });
+
+    }
+}
