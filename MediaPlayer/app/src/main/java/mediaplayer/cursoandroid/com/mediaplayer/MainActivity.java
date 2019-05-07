dc2f335eb9f2bb1b1aa476f4c489c8463feceae3
Lucas Oliveira <42073344+Storkluc@users.noreply.github.com>
2019-05-07 19:28:19 -0300

    LogMath Interface

--- MediaPlayer/app/src/main/java/mediaplayer/cursoandroid/com/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/mediaplayer/cursoandroid/com/mediaplayer/MainActivity.java
@@ -45,4 +45,14 @@ public class MainActivity extends Activity {
             botaoTocar.setText("Tocar");
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        if(mediaPlayer != null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    super.onDestroy();
+    }
 }
