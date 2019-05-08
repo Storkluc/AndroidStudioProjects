955dd39286f1aa89befd460954fc4ef64578313b
Lucas Oliveira <42073344+Storkluc@users.noreply.github.com>
2019-05-08 16:18:01 -0300

    LogMathLast

--- LogMath/app/src/main/java/logmath/com/logmath/ConfigActivity.java
+++ LogMath/app/src/main/java/logmath/com/logmath/ConfigActivity.java
@@ -1,6 +1,8 @@
 package logmath.com.logmath;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,21 +10,25 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class ConfigActivity extends AppCompatActivity {
+public class ConfigActivity extends Activity {
 
     private Button informarProblema;
     private RadioGroup radioGroup;
     private RadioButton radioButtonEscolhido;
+    private Button botaoSalvar;
     private CheckBox som;
+    private static final String ARQ_PREFERENCES = "ArqPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
 
-        informarProblema = (Button) findViewById(R.id.informarproblemaId);
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroupId);
+        informarProblema = findViewById(R.id.informarproblemaId);
+        radioGroup = findViewById(R.id.radioGroupId);
+        botaoSalvar = findViewById(R.id.botaoSalvarId);
 
         informarProblema.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +36,26 @@ public class ConfigActivity extends AppCompatActivity {
                 startActivity(new Intent(ConfigActivity.this,InformarProblemaActivity.class));
             }
         });
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int idBetaSelection = radioGroup.getCheckedRadioButtonId();
+
+                if(idBetaSelection>0){
+                    radioButtonEscolhido = findViewById(idBetaSelection);
+
+                    SharedPreferences sharedPreferences = getSharedPreferences(ARQ_PREFERENCES,0);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    String tipoSelecionado = radioButtonEscolhido.getText().toString();
+                    editor.putString("tipoEscolhido",radioButtonEscolhido.getText().toString());
+                    editor.commit();
+
+
+                }
+            }
+        });
+
     }
 }
